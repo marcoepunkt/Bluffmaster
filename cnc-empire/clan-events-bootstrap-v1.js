c3ded85b29a5f8193c177cd6fe2d9f87aff9fef3
marcoepunkt <m.endrijat@googlemail.com>
2026-09-17 14:07:11 +0200

    CNC: load winner reward enabled clan events

--- cnc-empire/clan-events-bootstrap-v1.js
+++ cnc-empire/clan-events-bootstrap-v1.js
@@ -1,4 +1,4 @@
 (async()=>{
   while(!window.CNC_ONLINE) await new Promise(r=>setTimeout(r,400));
-  try{await import('./clan-events-v1.js?v=2')}catch(e){console.error('Clan battle loader',e)}
+  try{await import('./clan-events-v1.js?v=3')}catch(e){console.error('Clan battle loader',e)}
 })();
