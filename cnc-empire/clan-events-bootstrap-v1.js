92c9197c80be06434844e26a85a5adccf9eed496
marcoepunkt <m.endrijat@googlemail.com>
2026-09-17 15:35:24 +0200

    Load clan cup hub and trophy cabinet

--- cnc-empire/clan-events-bootstrap-v1.js
+++ cnc-empire/clan-events-bootstrap-v1.js
@@ -1,7 +1,7 @@
 (async()=>{
   while(!window.CNC_ONLINE) await new Promise(r=>setTimeout(r,400));
   try{
-    await import('./clan-events-v1.js?v=3');
-    await import('./clan-reward-prestige-v1.js?v=1');
+    await import('./clan-events-v1.js?v=4');
+    await import('./clan-trophy-profile-v1.js?v=1');
   }catch(e){console.error('Clan battle loader',e)}
 })();
